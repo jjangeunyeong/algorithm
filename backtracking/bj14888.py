e26be555dd6e2ef89b2ccd84bb5ff14d61cb3cc8
Eunyeong Jang <jangeunyeong081@gmail.com>
2023-05-18 10:29:51 +0900

    Update bj14888.py

--- backtracking/bj14888.py
+++ backtracking/bj14888.py
@@ -5,25 +5,34 @@ add, sub, mul, div = map(int, input().split())  # (+,-.*,/) 개수
 maxAns = -1e9
 minAns = 1e9
 
+
 def sol(idx, num, add, sub, mul, div):
     global maxAns, minAns
-
+    print(1)
+    print(add, sub, mul, div)
     if idx == N:
+        print(2)
         maxAns = max(maxAns, num)
         minAns = min(minAns, num)
+        print(maxAns, minAns)
         return
 
     if add:
+        print(3)
         sol(idx+1, num+numArr[idx], add-1, sub, mul, div)
     if sub:
+        print(4)
         sol(idx+1, num-numArr[idx], add, sub-1, mul, div)
     if mul:
+        print(5)
         sol(idx+1, num*numArr[idx], add, sub, mul-1, div)
     if div:
+        print(6)
         if num < 0:
             sol(idx+1, -((-num)//numArr[idx]), add, sub, mul, div-1)
         else:
             sol(idx+1, num//numArr[idx], add, sub, mul, div-1)
+
 
 sol(1, numArr[0], add, sub, mul, div)
 print(maxAns)
